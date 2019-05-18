c54715174792cb05d0309752e1bd871f70ef07c3
FedeG <federicogonzalez761@gmail.com>
2019-05-18 15:34:47 -0300

    Remove code and add index with new workshops

--- src/practice/jsx/index.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>Hola, mundo!</h1>;
-ReactDOM.render(element, document.getElementById('root'));
